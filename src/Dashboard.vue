195d86590412ee61ab1a60dfae2835e0cf5731aa
khand <khand@stud.ntnu.no>
2018-01-12 17:14:33 +0100

    Dashboard

---
+++ src/Dashboard.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="dashboard">
+
+      <div class="block">
+        <div class="box">
+          <a>
+             <router-link to="/Test">prøving</router-link>
+          </a>
+          <a>
+            <router-link to="/HelloWorld">Hello</router-link>
+          </a>
+          <a>
+          <router-link to="/Task">Gjøremål</router-link>
+          </a>
+        </div>
+      </div>
+
+
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'dashboard'
+}
+</script>
+
+<style>
+#dashboard {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: black;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Test from '@/components/Test'
+import Task from '@/components/Task'
 
 Vue.use(Router)
 
@@ -10,6 +12,21 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
-    }
+    },
+    {
+      path: '/Test',
+      name: 'Test',
+      component: Test
+    },
+    {
+      path: '/HelloWorld',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/Task',
+      name: 'Task',
+      component: Task
+    },
   ]
 })
