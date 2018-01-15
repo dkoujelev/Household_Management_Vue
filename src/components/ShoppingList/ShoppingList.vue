c4e354e9e3c7d7fe79e86aca09fed06760425ab9
Eirik Stensen <stenseneirik@gmail.com>
2018-01-15 16:16:30 +0100

    Shopping liste

--- src/components/ShoppingList/ShoppingList.vue
+++ src/components/ShoppingList/ShoppingList.vue
@@ -18,11 +18,14 @@
 
                 <div class="field-body">
                   <input class="input" name="" v-model="row.name" type="text" placeholder="Text input" value="">
-                <button class="button is-danger" @click="row.antall += 1" id="decrementButton">
+                <button class="button is-danger" id="decrementButton" @click="decrement(row)">
                 <i class="fa fa-minus" id="minus" aria-hidden="true"></i>
                 </button>
-                  <button class="button is-info" @click="test" id="incrementButton">
+                  <button class="button is-info" @click="row.antall++" id="incrementButton">
                   <i class="fa fa-plus" id="plus" aria-hidden="true" ></i>
+                    <div>
+                      {{row.antall}}
+                    </div>
                   </button>
                 </div>
 
@@ -34,12 +37,11 @@
                   <button class="button is-danger" @click="addRow">Add</button>
                   <button class="button is-danger" @click="removeRow(row)">Remove</button>
 
-
                 </div>
               </div>
               <div class="field is-grouped">
                 <div class="control">
-                  <button class="button is-link">Submit</button>
+                  <button class="button is-link" @click="sendData">Submit</button>
                 </div>
                 <p class="control">
                   <a class="button is-danger">
@@ -49,18 +51,26 @@
               </div>
             </div>
 
+
           </article>
         </div>
       </div>
     </div>
   </div>
-  </template>
+
+
+</template>
+
 
 
 
 <script>
   //metode som inneholder et objekt som returnern data
+
+  import axios from 'axios';
+
   export default {
+
     name: "ShoppingList",
     data() {
       return {
@@ -71,15 +81,36 @@
     },
     methods: {
       addRow: function () {
-        this.rows.push({antall:1});
+        this.rows.push({name: "", antall: 1});
       },
       removeRow: function (row) {
         //console.log(row);
         this.rows.splice(row, 1);
       },
-      test(){
-        alert("test");
+      decrement(row){
+        if(row.antall > 1){
+          row.antall--;
+        }
+      },
+
+      sendData(){
+        let shoppinglist =
+        {
+          navn: this.name,
+          opprettet: new Date(),
+          frist: null,
+          beskrivelse: "",
+          undergruppe_id: 1, //Må leses inn fra komponent (foreldre som inneholder)
+          handling_utfort: null,
+          varer: this.row
+        };
+
+        axios.post('http://localhost:9000/rest/handleliste', shoppinglist, response => {
+          alert('handleliste lagt inn!');
+        });
+
       }
+
     }
   }
 
