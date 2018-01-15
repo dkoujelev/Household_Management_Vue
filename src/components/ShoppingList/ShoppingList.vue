237322b3b112ae3b57da6128802fe073fb0a47b5
Eirik Stensen <stenseneirik@gmail.com>
2018-01-15 18:21:23 +0100

    Merge remote-tracking branch 'remotes/origin/odd' into ShoppingList
    # Conflicts:
    #	package-lock.json

--- src/components/ShoppingList/ShoppingList.vue
+++ src/components/ShoppingList/ShoppingList.vue
@@ -3,7 +3,7 @@
     <div class="tile is-vertical is-8">
       <div class="tile">
         <div class="tile is-parent is-vertical">
-          <article class="tile is-child notification is-primary">
+          <article class="tile is-child notification">
             <div class="container is-fluid">
               <div class="field">
                 <label class="label">Navn å vareliste</label>
@@ -12,46 +12,44 @@
                 </div>
               </div>
 
-
+              <label class="label">Skriv inn varer</label>
               <div class="field" v-for="row in rows">
-                <!--<label class="label">Skriv inn varer</label> -->
+
 
                 <div class="field-body">
-                  <input class="input" name="" v-model="row.name" type="text" placeholder="Text input" value="">
-                <button class="button is-danger" id="decrementButton" @click="decrement(row)">
-                <i class="fa fa-minus" id="minus" aria-hidden="true"></i>
-                </button>
+                  <button class="button is-danger" id="decrementButton" @click="decrement(row)">
+                    <i class="fa fa-minus" id="minus" aria-hidden="true"></i>
+                  </button>
+                  <input class="input" name="" v-model="row.navn" type="text" placeholder="Text input" value="">
                   <button class="button is-info" @click="row.antall++" id="incrementButton">
                   <i class="fa fa-plus" id="plus" aria-hidden="true" ></i>
+                  </button>
+                    <button class="button is-danger" @click="removeRow(row)">
+                      <i class="fa fa-trash-o" aria-hidden="true"></i>
+                    </button>
                     <div>
                       {{row.antall}}
                     </div>
-                  </button>
-                </div>
 
+                </div>
               </div>
 
               <div class="field is-grouped">
                 <div class="control">
-
-                  <button class="button is-danger" @click="addRow">Add</button>
-                  <button class="button is-danger" @click="removeRow(row)">Remove</button>
-
+                  <button class="button is-success" @click="addRow">Add</button>
                 </div>
-              </div>
-              <div class="field is-grouped">
-                <div class="control">
-                  <button class="button is-link" @click="sendData">Submit</button>
-                </div>
-                <p class="control">
-                  <a class="button is-danger">
-                    Delete post
-                  </a>
-                </p>
               </div>
             </div>
-
-
+            <div class="field">
+              <label class="label">Message</label>
+              <div class="control">
+                <textarea class="textarea" placeholder="Textarea"></textarea>
+              </div>
+            </div>
+            <div class="control">
+              <button class="button is-link" @click="sendData">Godkjenn</button>
+              <button class="button is-danger" @click="">Slett handleliste</button>
+            </div>
           </article>
         </div>
       </div>
@@ -71,17 +69,18 @@
 
   export default {
 
-    name: "ShoppingList",
+
     data() {
       return {
+        name: "handle",
         rows: [
-          {name: "", antall: 1},
+          {navn: "", antall: 1},
         ]
       }
     },
     methods: {
       addRow: function () {
-        this.rows.push({name: "", antall: 1});
+        this.rows.push({navn: "", antall: 1});
       },
       removeRow: function (row) {
         //console.log(row);
@@ -100,17 +99,19 @@
           opprettet: new Date(),
           frist: null,
           beskrivelse: "",
-          undergruppe_id: 1, //Må leses inn fra komponent (foreldre som inneholder)
+          undergruppe_id: 2, //Må leses inn fra komponent (foreldre som inneholder)
           handling_utfort: null,
-          varer: this.row
+          varer: this.rows
         };
+        console.dir(shoppinglist);
 
         axios.post('http://localhost:9000/rest/handleliste', shoppinglist, response => {
           alert('handleliste lagt inn!');
+        }).catch(err => {
+          console.log(JSON.stringify(err));
         });
 
       }
-
     }
   }
 
@@ -128,6 +129,7 @@
 #plus{
   color: black;
   }
+
 
 
 </style>
