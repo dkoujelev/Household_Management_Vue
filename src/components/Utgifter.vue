467f565ce9b364f112bddd06a2474f54d8103893
Dmitryko97 <dkoujelev97@gmail.com>
2018-01-17 08:44:18 +0100

    Added vue-good-table

---
+++ src/components/Utgifter.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <vue-good-table
+      title="Utgifter"
+      :columns="columns"
+      :rows="rows"
+      :paginate="true"
+      per-page=5
+      next-text="Neste"
+      prev-text="Forrige"
+      rows-per-page-text="Antall rader"
+      />
+  </div>
+</template>
+
+<script>
+  import axios from 'axios';
+  import Vue from 'vue'
+  import VueGoodTable from 'vue-good-table';
+
+  Vue.use(VueGoodTable);
+
+  export default {
+    name: 'utgifter',
+    data(){
+      return {
+        columns: [
+          {
+            label: 'Kostnad',
+            field: 'kostnad',
+            filterable: true
+          },
+          {
+            label: 'Sum',
+            field: 'sum',
+            type: 'number',
+            html: false,
+            filterable: false
+          }
+        ],
+        rows: []
+      };
+    },
+    mounted(){
+      this.fillRows();
+    },
+    methods: {
+      fillRows(){
+        axios.get('http://localhost:9000/rest/melding/sendt/bruker/1').then(response => {
+          let resRows = response.data;
+          for(let i = 0; i < resRows.length; i++){
+            let obj = {kostnad: resRows[i].overskrift, sum: resRows[i].tekst};
+            this.rows.push(obj);
+          }
+        }).catch(err => {
+          console.log(err);
+        });
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
