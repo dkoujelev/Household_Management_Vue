2ff97e29dbc0b5a564ed439530411f4d57cf6213
odd <oddbjool@stud.ntnu.no>
2018-01-17 12:46:55 +0100

    REST API for spesifisert gjeld (fra bruker til bruker)

--- backend/REST/resources/Gjeld.js
+++ backend/REST/resources/Gjeld.js
@@ -30,3 +30,17 @@ server.get('rest/gjeldBrukerKreverInn/:bruker_id',function(req,res,next) {
     return next();
   });
 });
+
+// Henter all gjeld som er registrert fra en bestemt bruker til en annen bestemt bruker
+// For å hente all gjeld fra bruker 1 til bruker 2, send følgende i request body:
+// {skylder: 1, innkrever: 2}
+server.post('rest/gjeldSpesifisert', function(req,res,next) {
+  connection.query('SELECT * FROM Gjeld WHERE Gjeld.bruker_skylder_id=? ' +
+    'AND Gjeld.bruker_innkrever_id=?', [req.body.skylder, req.body.innkrever], function (err, rows, fields) {
+    if (err)
+      return next(err);
+
+    res.send(rows);
+    return next();
+  });
+});
