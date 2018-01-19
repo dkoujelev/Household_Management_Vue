65bc03e7e7116891022f685048f123f9719367a0
khand <khand@stud.ntnu.no>
2018-01-19 12:31:44 +0100

    NewUser style

--- src/components/NewUser.vue
+++ src/components/NewUser.vue
@@ -1,14 +1,24 @@
 <template>
-  <div>
-    <p>Velkoment til vår nettside!</p>
-    <p>Du er ikke med i noe kolletiv enda</p>
-    <br/>
-    <p>Hvis kollektivet ditt allerede er opprettet kan du søke om å bli med i det her:</p>
-    <router-link class="button" to="Innmelding">Finn kollektiv</router-link>
-    <br/>
-    <p>Hvis du ønsker å opprette ett nytt kollektiv kan du gjøre det her:</p>
-    <router-link class="button" to="">Opprett kollektiv</router-link> <!--To create new collectiv page when ready-->
+  <div class="is-ancestor">
+    <div class="is-parent tile is-vertical box" style="background-color: azure">
+      <div class="is-child tile is is-vertical">
+        <h1 class="tittle is-3">Velkommen til vår nettside!</h1>
+        <p class="content is-medium">Du er ikke med i noe kolletiv enda</p>
+      </div>
+      <div class="is-child tile">
+        <p>Hvis kollektivet ditt allerede er opprettet kan du søke om å bli med i det her:</p>
+        <router-link class="button is-dark" to="Innmelding">Finn kollektiv</router-link>
+      </div>
+      <div class="is-child tile">
+        <p>Hvis kollektivet ditt allerede er opprettet kan du søke om å bli med i det her:</p>
+        <router-link class="button is-dark" to="">Opprett kollektiv</router-link> <!--To create new collectiv page when ready-->
+      </div>
+    </div>
   </div>
+
+
+
+
 </template>
 
 <script>
