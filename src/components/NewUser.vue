7250cfad025c753efcda250d406e25634e48ba81
Dmitryko97 <dkoujelev97@gmail.com>
2018-01-19 10:45:04 +0100

    New user

---
+++ src/components/NewUser.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <p>Velkoment til vår nettside!</p>
+    <p>Du er ikke med i noe kolletiv enda</p>
+    <br/>
+    <p>Hvis kollektivet ditt allerede er opprettet kan du søke om å bli med i det her:</p>
+    <router-link class="button" to="Innmelding">Finn kollektiv</router-link>
+    <br/>
+    <p>Hvis du ønsker å opprette ett nytt kollektiv kan du gjøre det her:</p>
+    <router-link class="button" to="">Opprett kollektiv</router-link> //To create new collectiv page when ready
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "new-user"
+    }
+</script>
+
+<style scoped>
+
+</style>
