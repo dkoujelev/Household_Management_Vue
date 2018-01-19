612175fc29dedba7b82c7d6d85aac7da898256b0
Dmitryko97 <dkoujelev97@gmail.com>
2018-01-19 14:20:31 +0100

    hotfix change password

--- src/components/ChangePassword.vue
+++ src/components/ChangePassword.vue
@@ -2,6 +2,7 @@
   <div>
     <h1>Endring av passord</h1>
     <br/>
+    <p class="help is-danger">{{this.error.all}}</p>
     <p class="help is-danger">{{this.error.oldPassword}}</p>
     <input type="password" placeholder="Gammelt passord" v-model="oldPassword"/>
     <br/>
@@ -30,54 +31,62 @@
           },
           error: {
             oldPassword: '',
-            newPasswordMissmatch: ''
+            newPasswordMissmatch: '',
+            all: ''
           }
         }
       },
       methods: {
-          clearFields(){
-            this.oldPassword = '';
-            this.newPassword = '';
-            this.newPasswordRepeat = '';
-          },
+        clearFields(){
+          this.oldPassword = '';
+          this.newPassword = '';
+          this.newPasswordRepeat = '';
+        },
         clearErrors(){
           this.error.newPasswordMissmatch = '';
           this.error.oldPassword = '';
+          this.error.all = '';
         },
-          validateInfo(){
-            let isValid = true;
-            clearErrors();
+        validateInfo(){
+          let isValid = true;
+          this.clearErrors();
 
-            if(this.newPassword != this.newPasswordRepeat){
-              clearFields();
-              this.error.newPasswordMissmatch = 'Passordene er ikke like';
+          if(this.newPassword === '' || this.oldPassword === '' || this.newPasswordRepeat === ''){
+            this.error.all = 'Ingen felter kan stå tomme';
+            isValid = false;
+          }
+
+          if(this.newPassword !== this.newPasswordRepeat){
+            this.clearFields();
+            this.error.newPasswordMissmatch = 'Passordene er ikke like';
+            isValid = false;
+          }
+
+          this.infoToValidate.password = this.oldPassword;
+          axios.post('http://localhost:9000/rest/password', this.infoToValidate).then(response => {
+            if(!response.data.valid){
+              this.clearFields();
+              this.error.oldPassword = 'Feil passord';
               isValid = false;
             }
+          });
 
-            this.infoToValidate.password = this.oldPassword;
-            axios.post('http://localhost:9000/rest/password', this.infoToValidate).then(response => {
-              if(!response.data.valid){
-                clearFields();
-                this.error.oldPassword = 'Feil passord';
-                isValid = false;
+          return isValid;
+        },
+        changePassword(){
+          if(this.validateInfo()){
+            let update = {newPassword: this.newPassword};
+            axios.put('http://localhost:9000/rest/changePassword', update).then(response => {
+              if(response.data.updated){
+                alert('changed');
+                this.$emit('passwordUpdated');
+                router.push(''); // Push to my profile page
+              } else{
+                this.$emit('passwordChangeFailed');
               }
             });
-
-            return isValid;
-          },
-          changePassword(){
-            if(validateInfo()){
-              let update = {newPassword: this.newPassword};
-              axios.post('http://localhost:9000/rest/changePassword', update).then(response => {
-                if(response.data.updated){
-                  this.$emit('passwordUpdated');
-                  router.push(''); // Push to my profile page
-                } else{
-                  this.$emit('passwordChangeFiled');
-                }
-              });
-            }
           }
+        }
       }
     }
 </script>
