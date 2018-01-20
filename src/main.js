a02c5b31382c20362b5a8a1ae5bb3fe0403c088b
odd <olsen.oddbjorn@gmail.com>
2018-01-20 16:44:32 +0100

    Redirect til login om man ikke er logget inn

--- src/main.js
+++ src/main.js
@@ -11,6 +11,35 @@ window.Vue = Vue;
 
 axios.defaults.withCredentials = true;
 
+router.beforeEach((to,from,next) => {
+
+  if(to.path === '/Login' || to.path === '/Registrer'){
+    console.log("router.beforeEach: user is already on login/register page, no redirect");
+    next();
+  }
+
+
+  if(store.state.loggedIn){
+    console.log("router.beforeEach: already logged in, don't redirect");
+    next();
+  }
+
+  axios.get('http://localhost:9000/rest/loggedIn').then(response => {
+    if(response.data !== null){
+      console.log("router.beforeEach: User is logged in, proceed with route");
+      store.state.current_user = response.data;
+      store.state.loggedIn = true;
+      next();
+    }
+    else{
+      console.log("router.beforeEach: User isn't logged in! Sending user to Login");
+      next('Login');
+    }
+
+  });
+
+});
+
 /* eslint-disable no-new */
 new Vue({
   el: '#dashboard',
