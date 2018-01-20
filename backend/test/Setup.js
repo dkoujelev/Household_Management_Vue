d4a4ea25f9df39b33c2cdadbecb816915267e377
odd <olsen.oddbjorn@gmail.com>
2018-01-20 22:36:58 +0100

    Fake REST login to make tests work (super hack)

--- backend/test/Setup.js
+++ backend/test/Setup.js
@@ -1,6 +1,14 @@
 let clearDB = require('./clearDB');
+let axios = require('axios');
+let auth = require('../REST/auth');
 
 before((done) => {
+
+  // We have to FAKE LOGIN against REST, or most tests will produce 404.
+  // Extreme hack.
+
+  let sessionId = auth.newSession("fake user");
+  axios.defaults.headers.common['cookie'] = 'sessionId='+sessionId;
   require("../REST/runserver_test")(() => done());
 });
 
