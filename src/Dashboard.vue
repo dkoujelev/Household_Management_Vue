80783f5da65380a71e3b6f44cb08d4032b0ea0b4
khand <khand@stud.ntnu.no>
2018-01-22 14:03:44 +0100

    merge ing

--- src/Dashboard.vue
+++ src/Dashboard.vue
@@ -25,10 +25,11 @@
               <router-link class="navbar-item" v-if="showNav" to="/TestMail">TestMail</router-link>
               <router-link class="navbar-item" v-if="showNav" to="/GjeldInn">Gjeld Inn</router-link>
               <router-link class="navbar-item" v-if="showNav" to="/GjeldUt">Gjeld Ut</router-link>
-              <router-link class="navbar-item" v-if="showNav" to="/UserInfo">Profil</router-link>
+              <router-link class="navbar-item" v-if="showNav" to="/UserInfo">Min Side</router-link>
             </div>
 
             <div class="navbar-end" @click="showBurger = false">
+              <SelectGroup :current_user="current_user" @selected-group="selectedGroup" ref="SelectGroup"></SelectGroup>
               <a href="" class="navbar-item" v-if="loggedIn" @click.prevent="logOut">Logg Ut</a>
             </div>
           </div>
