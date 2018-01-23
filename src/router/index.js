ed2bc89bcdfe4409f4bbbd87786750495086958a
KimRostgard <max.ntnu@gmail.com>
2018-01-23 13:01:25 +0100

    Dashboard, routing.js and ShowAccounting added/ updated

--- src/router/index.js
+++ src/router/index.js
@@ -24,6 +24,7 @@ import NewUser from '@/components/NewUser'
 import ChangePassword from '@/components/ChangePassword'
 import Grupper from '@/components/Grupper'
 import UserInfo from '@/components/UserInfo'
+import ShowAccounting from '@/components/ShowAccounting.vue'
 
 
 Vue.use(Router);
@@ -155,7 +156,12 @@ export default new Router({
         path: '/UserInfo',
         name: 'UserInfo',
         component: UserInfo
-      }
+      },
+      {
+        path: '/ShowAccounting',
+        name: 'ShowAccounting',
+        component: ShowAccounting
+      },
     ]
   }
 )
