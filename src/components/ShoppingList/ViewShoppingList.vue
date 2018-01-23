9a3892a888762b0360efabb6c5a246780dbadbe0
Dmitryko97 <dkoujelev97@gmail.com>
2018-01-23 15:45:17 +0100

    update shoppinglist

--- src/components/ShoppingList/ViewShoppingList.vue
+++ src/components/ShoppingList/ViewShoppingList.vue
@@ -13,9 +13,30 @@
         <td><button class="button">Fjern vare</button></td>
       </tr>
     </table>
-    <br />
-    <button class="button">Legg til vare</button>
-    <button class="button is-danger">Slett liste</button>
+
+    <div v-if="addItem">
+      <button class="button is-danger" @click="hide">
+        <i class="fa fa-trash-o" aria-hidden="true"></i>
+      </button>
+      <input class="input" name="" v-model="newItem.name" type="text" placeholder="Text input" value="">
+      <button class="button is-danger" id="decrementButton" @click="decrement()">
+        <i class="fa fa-minus" id="minus" aria-hidden="true"></i>
+      </button>
+      <div>
+        <button class="button is-light">{{newItem.count}}</button>
+      </div>
+      <button class="button is-info" @click="increment()" id="incrementButton">
+        <i class="fa fa-plus" id="plus" aria-hidden="true" ></i>
+      </button>
+      <button class="button is-success" @click="updateList">
+        Legg til vare
+      </button>
+    </div>
+
+    <br/>
+    <button class="button" v-if="!addItem" @click="addItem = true">Legg til vare</button>
+    <br/>
+    <button class="button is-danger" @click="deleteList">Slett handleliste</button>
   </div>
 </template>
 
@@ -27,14 +48,45 @@
 
     data(){
       return {
-        rows: []
+        rows: [],
+        addItem: false,
+        newItem: {
+          name: '',
+          count: 1
+        }
       };
     },
     mounted(){
       this.fillRows();
     },
     methods: {
+      hide(){
+        this.addItem = false;
+        this.newItem.name = '';
+        this.newItem.count = 1;
+      },
+      increment(){
+        this.newItem.count++;
+      },
+      decrement(){
+        if(this.newItem.count > 1) this.newItem.count--;
+      },
+      updateList(){
+        let obj = {
+          handleliste_id: Number.parseInt(this.$route.params.shoppinglist_id),
+          navn: this.newItem.name,
+          antall: this.newItem.count
+        };
 
+        axios.post('http://localhost:9000/rest/vare', obj).then(response => {
+          this.$emit('ItemAdded', obj);
+          this.rows.push({vare: this.newItem.name, antall: this.newItem.count});
+          this.hide();
+        });
+      },
+      deleteList(){
+
+      },
       fillRows(){
         axios.get('http://localhost:9000/rest/handleliste/' + this.$route.params.shoppinglist_id).then(response => {
           console.log(response.data);
