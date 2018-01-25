1f47bc7043f625e72ac3b49fd2097bd07c35111b
frodeuh <frodeuh@ntnu.no>
2018-01-25 14:25:29 +0100

    Created *Statistikk.js

---
+++ backend/REST/resources/Statistikk.js
@@ -0,0 +1,28 @@
+module.exports = function(connection, server) {
+  // Hent statistikk over alle kjøpte varer i en undergruppe (NOT USED)
+  server.get('rest/statistikkMestKjopteVarerUndergruppe/:undergruppe_id', function (req, res, next) {
+    connection.query('SELECT * FROM Vare INNER JOIN Handleliste ON Vare.handleiste_id = Handleliste.handleiste_id ' +
+      'WHERE undergruppe_id=? AND handling_utfort IS NOT NULL', req.params.undergruppe_id, function (err, rows, fields) {
+      if(err)
+        return next(err);
+      res.send(rows);
+    });
+  });
+
+  // Hent statistikk over alle kjøpte varer i et kollektiv
+  server.get('rest/statistikkMestKjopteVarerKollektiv/:kollektiv_id', function (req, res, next) {
+    if('fra' in req.body)
+      req.body.fra = new Date(req.body.fra).getTime();
+    if('til' in req.body)
+      req.body.til = new Date(req.body.til).getTime();
+    connection.query('SELECT DISTINCT Vare.navn, sum(antall) AS antall FROM `Vare` ' +
+      'INNER JOIN Handleliste ON Vare.handleliste_id = Handleliste.handleliste_id ' +
+      'INNER JOIN Undergruppe ON Handleliste.undergruppe_id = Undergruppe.undergruppe_id ' +
+      'WHERE kollektiv_id=? AND Handleliste.handling_utfort >=? AND Handleliste.handling_utfort <=? ' +
+      'GROUP by (Vare.navn) ORDER BY antall DESC LIMIT ?', [req.params.kollektiv_id, req.body.fra, req.body.til, req.body.antall], function (err, rows, fields) {
+      if(err)
+        return next(err);
+      res.send(rows);
+    });
+  });
+};
