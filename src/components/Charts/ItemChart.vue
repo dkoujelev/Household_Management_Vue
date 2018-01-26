e140cc975a8d674d7f7724ca9b180febfae91cdb
odd <oddbjool@stud.ntnu.no>
2018-01-26 09:18:58 +0100

    Fungerende ItemChart (oftest kjøpte varer)

--- src/components/Charts/ItemChart.vue
+++ src/components/Charts/ItemChart.vue
@@ -1,9 +1,18 @@
 <template>
-    <div class="box">
-      <div class='chart'>
-        <div class="field"><label>test</label><flat-pickr v-model="from"></flat-pickr></div>
-        <div class="field"><label>test</label><flat-pickr v-model="to"></flat-pickr></div>
-        <ChartAxis :data='chartData'></ChartAxis>
+    <div class="is-parent">
+      <div class="tile is-child box">
+        <div class='chart'>
+          <div class="field"><label>fra dato: </label><flat-pickr v-model="from"></flat-pickr></div>
+          <div class="field"><label>til dato: </label><flat-pickr v-model="to"></flat-pickr></div>
+          Oftest kjøpte varer i valgt periode:
+          <ChartAxis :data='chartData'></ChartAxis>
+        </div>
+      </div>
+      <!-- FUCKED FIX for å unngå at chart havner utfor div'en. -->
+      <div class="tile is-child box">
+        <br />
+        <br />
+        <br />
       </div>
     </div>
 </template>
@@ -14,51 +23,70 @@
   import flatPickr from 'vue-flatpickr-component';
   import 'flatpickr/dist/flatpickr.css';
   import {store} from '@/store';
+  import axios from 'axios';
 
   export default {
     data () {
+      let from_date = new Date();
+      from_date.setFullYear(from_date.getFullYear()-1);
       return {
-        from: new Date(),
+        from: from_date,
         to: new Date()
       }
     },
     asyncComputed:{
+      chartData: {
+        get(){
+          let settings = {
+            fra: this.from, til:this.to,
+            kollektiv_id: store.state.current_group.kollektiv_id,
+            antall: 5
+          };
 
-      chartData(){
-        let settings = {
-          from: this.from, to:this.to,
-          kollektiv_id: store.state.current_group.kollektiv_id,
-          antall: 5
-        };
+          let response = {data:[
+            {navn: 'Melk', antall:3},
+            {navn: 'Ost', antall: 5}
+          ]};
 
-        let response = {data:[
-          {navn: 'Melk', antall:3},
-          {navn: 'Ost', antall: 5}
-        ]};
-
-//        return axios.post('http://localhost:9000/rest/oftestKjopteVarer', settings)
-//          .then(response => {
-            let chart_data= {
-              sets: [
-                {
-                  generators: [svgBar()],
-                  graphs: [
-                    {
-                      label: 'Ofte kjøpte varer',
-                      values: []
-                    }
-                  ]
-                }
-              ]
-            };
-            for(let vare of response.data){
-              chart_data.sets[0].graphs[0].values.push({
-                x: vare.navn,
-                y: vare.antall
-              });
-            }
-            return chart_data;
-//          });
+        return axios.post('http://localhost:9000/rest/statistikkMestKjopteVarerKollektiv/'+settings.kollektiv_id, settings)
+          .then(response => {
+          let chart_data= {
+            sets: [
+              {
+                generators: [svgBar()],
+                graphs: [
+                  {
+                    label: 'Ofte kjøpte varer',
+                    values: []
+                  }
+                ]
+              }
+            ]
+          };
+          for(let vare of response.data){
+            chart_data.sets[0].graphs[0].values.push({
+              x: vare.navn,
+              y: vare.antall
+            });
+          }
+          return chart_data;
+          });
+        },
+        default(){
+          return {
+            sets: [
+              {
+                generators: [svgBar()],
+                graphs: [
+                  {
+                    label: 'Ofte kjøpte varer',
+                    values: []
+                  }
+                ]
+              }
+            ]
+          };
+        }
       }
     },
     components: {
@@ -71,6 +99,6 @@
 <style scoped>
   .chart{
     width: 100%;
-    height: 500px;
+    height: 400px;
   }
 </style>
